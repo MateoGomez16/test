f5d24b13eb076c81ebcedaa69a8a6ede12d96b69
MateoGomez16 <mateogomez0416@outlook.com>
2025-08-11 14:11:40 -0500

    Update timer trigger schedule to run every 5 minutes and enhance logging for scraping functionality

--- function_app.py
+++ function_app.py
@@ -1,11 +1,12 @@
 import datetime
 import logging
 import azure.functions as func
+from playwright.sync_api import sync_playwright
 
 app = func.FunctionApp()
 
 @app.function_name(name="mytimer")
-@app.timer_trigger(schedule="*/1 * * * * *",  # Cada minuto
+@app.timer_trigger(schedule="*/5 * * * * *",  # Cada minuto
                    arg_name="mytimer",
                    run_on_startup=False)
 def run_monthly(mytimer: func.TimerRequest) -> None:
@@ -17,5 +18,16 @@ def run_monthly(mytimer: func.TimerRequest) -> None:
     
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
     print("Hello, World!")
+
+    try:
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+            page.goto("https://portal.zondahome.com/")
+            data = page.inner_text("body")
+            logging.info(f"Contenido de la página: {data[:200]}...")  # solo 200 caracteres
+            browser.close()
+    except Exception as e:
+        logging.error(f"Error en el scraping: {str(e)}")
 
     # Aquí va tu código de descarga y procesamiento
